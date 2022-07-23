2cc1944c324d89867f56083af9bf2ef6bb9c2915
Johanes Rivaldo <98978269+johanesrivaldo@users.noreply.github.com>
2022-07-23 16:06:08 +0700

    Add files via upload
    import databasesnya terlebih dahulu untuk mengverifikasi kata sandi user dan opertaor pada halaman login website,terimaksih

---
+++ index.php
@@ -0,0 +1,77 @@
+<!doctype html>
+<html lang="en" class="h-100">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Hugo 0.84.0">
+    <link rel="shortcut icon" href="img/icons/icon-48x48.png" />
+    <title>PT. NIKI FOUR</title>
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/5.0/examples/cover/">
+
+
+
+    <!-- Bootstrap core CSS -->
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+
+    <style>
+    .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        user-select: none;
+    }
+
+    @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+            font-size: 3.5rem;
+        }
+    }
+    </style>
+
+
+    <!-- Custom styles for this template -->
+    <link href="assets/css/cover.css" rel="stylesheet">
+</head>
+
+<body class="d-flex h-100 text-center text-white " style="background-image: url(assets/img/bg011.jpg); background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;">
+
+    <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+        <header class="mb-auto">
+            <div>
+
+            </div>
+        </header>
+
+        <main class="px-2">
+
+            <h1>PT. NIKI FOUR</h1><BR>
+
+            <b>
+                <p class="lead">Jl. Proklamasi No. 241,
+                    Jati Ilir II RT.01/06, Kel. Tunggak- Jati Kec. Karawang Barat, Karawang – Jawa Barat Indonesia 41351
+                </p>
+            </b>
+            <p class=" lead">
+                <a href="login.php" class="btn btn-lg btn-secondary fw-bold border-white bg-white">Login</a>
+            </p>
+        </main>
+
+        <footer class="mt-auto text-white-50">
+            <p><a class="text-white">Mohammad Khatar Malayki</a>,  <a class="text-white">19101140)</a>.</p>
+            <p> <a class="text-white">Khasanatun</a>,  <a class="text-white">191011401212)</a>.</p>
+            <p> <a class="text-white">Yohanes Rivaldi Jemadu</a>,  <a class="text-white">(191011401199)</a>.</p>
+        </footer>
+    </div>
+
+
+
+</body>
+
+</html>
